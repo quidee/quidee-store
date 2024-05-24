6f5af29063350c452ed78a73469ca7ef9260a050
Gabriela <gabriela.cleim@quideedelivery.com>
2024-05-24 10:48:38 -0300

    splash screen without logo

---
+++ app/src/main/java/com/example/quideestore/SplashScreenActivity.java
@@ -0,0 +1,20 @@
+package com.example.quideestore;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        startActivity(new Intent(SplashScreenActivity.this, MainActivity.class));
+        finish();
+    }
+}
