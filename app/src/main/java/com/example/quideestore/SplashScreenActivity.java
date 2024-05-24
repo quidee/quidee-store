bee034d13d533937a44e11c2db5be19d7c3323e1
Gabriela <gabriela.cleim@quideedelivery.com>
2024-05-24 11:05:41 -0300

    splash screen with logo

--- app/src/main/java/com/example/quideestore/SplashScreenActivity.java
+++ app/src/main/java/com/example/quideestore/SplashScreenActivity.java
@@ -1,13 +1,7 @@
 package com.example.quideestore;
-
 import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
