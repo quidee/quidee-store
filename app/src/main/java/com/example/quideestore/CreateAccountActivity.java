b758608682539506e057a752221d3f707a87c742
Gabriela <gabriela.cleim@quideedelivery.com>
2024-05-27 10:48:02 -0300

    create account screen ok

--- app/src/main/java/com/example/quideestore/CreateAccountActivity.java
+++ app/src/main/java/com/example/quideestore/CreateAccountActivity.java
@@ -1,19 +1,20 @@
 package com.example.quideestore;
 
 import android.os.Bundle;
+
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
 
-public class LoginScreenActivity extends AppCompatActivity {
+public class CreateAccountActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_screen_login);
+        setContentView(R.layout.activity_create_account);
+
 
         //TODO: Create all the logic
-
     }
 }
