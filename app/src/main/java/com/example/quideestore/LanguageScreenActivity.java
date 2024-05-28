622b16f8ee4faaa1b36df4828983e42dffea367a
Gabriela <gabriela.cleim@quideedelivery.com>
2024-05-28 11:16:56 -0300

    language screen ok

--- app/src/main/java/com/example/quideestore/LanguageScreenActivity.java
+++ app/src/main/java/com/example/quideestore/LanguageScreenActivity.java
@@ -1,20 +1,19 @@
 package com.example.quideestore;
 
-
 import android.os.Bundle;
+
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
-
-public class LoginScreenActivity extends AppCompatActivity {
+public class LanguageScreenActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_screen_login);
+        setContentView(R.layout.activity_language_screen);
 
-        //TODO: Create all the logic
+
 
     }
 }
