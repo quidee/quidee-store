e807ae8e15b9233ea10c6c2dea97d3221006ad60
Gabriela <gabriela.cleim@quideedelivery.com>
2024-06-03 09:48:38 -0300

    edit profile screen ok

--- app/src/main/java/com/example/quideestore/EditProfileActivity.java
+++ app/src/main/java/com/example/quideestore/EditProfileActivity.java
@@ -1,9 +1,7 @@
 package com.example.quideestore;
 
-
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
+
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,33 +9,22 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-
-public class LoginScreenActivity extends AppCompatActivity {
-
-    Button btnLogin;
+public class EditProfileActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_screen_login);
+        setContentView(R.layout.activity_edit_profile);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-        //TODO: Create all the logic
-
-        btnLogin = findViewById(R.id.btn_sign_in);
-
-        //to test
-        btnLogin.setOnClickListener(v -> {
-            Intent intent = new Intent(LoginScreenActivity.this, EditProfileActivity.class);
-            startActivity(intent);
-        });
-
 
 
     }
+
+
 }
