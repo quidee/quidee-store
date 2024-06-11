0d01964c0a356e5a13150c482b647857e393b5cc
Gabriela <gabriela.cleim@quideedelivery.com>
2024-06-11 14:27:48 -0300

    Cart screen ok

--- app/src/main/java/com/example/quideestore/CartScreenActivity.java
+++ app/src/main/java/com/example/quideestore/CartScreenActivity.java
@@ -14,7 +14,7 @@ public class CartScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_search_screen);
+        setContentView(R.layout.activity_cart_screen);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
